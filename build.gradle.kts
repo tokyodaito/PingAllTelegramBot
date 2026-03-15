4a24897da65ef60a28f7f2851146a959392f8a28
Bogdan <bogdan.bogotov123123@gmail.com>
2026-03-15 19:06:57 +0400

    fix logs

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:3.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("org.xerial:sqlite-jdbc:3.47.2.0")
+    runtimeOnly("org.slf4j:slf4j-jdk14:2.0.17")
 
     testImplementation(kotlin("test"))
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
